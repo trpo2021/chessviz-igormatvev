b6a79e42edff619ba1d0d395d3b5acd690e4f951
igormatvev <78473335+igormatvev@users.noreply.github.com>
2021-05-01 20:34:21 +0700

    Delete bibl.h

--- bibl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void PrintBoard(char Board[11][11]);
-
-void Figures(char Board[11][11]);
-
-void CellBOARD(char Board[11][11]);
-
-void Move(char Board[11][11], char FiguresP[5], int countar[8], int countar1[8]);
