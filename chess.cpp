68d08b7a8b9435fd91ee4c829f62b7291dbfb9bc
igormatvev <78473335+igormatvev@users.noreply.github.com>
2021-05-01 19:08:06 +0700

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include "bibl.h"
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    int countar[8], countar1[8];
+    for(int i = 0; i < 8; ++i) countar[i] = 0;
     char Board[11][11];
     CellBOARD(Board);
     char FiguresP[5];
@@ -10,7 +12,7 @@ int main()
         std::cout << "Введите координату клетки нужной фигуры и координату клетки 'назначения'(пример: E2-E5): " << std::endl;
         for(int i = 0; i < 5; ++i)
             std::cin >> FiguresP[i];
-        Move(Board, FiguresP);
+        Move(Board, FiguresP, countar, countar1);
     }
     return 0;
 }
